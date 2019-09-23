8b6ad8d13cf441c56ae7ccebfba1d3b21da7ad5e
Oscar Bonilla <6f6231@gmail.com>
2019-09-23 14:09:37 -0700

    Fix a race and more pretty

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 	"runtime"
 )
 
-type Result struct {
+type result struct {
 	result bool
 	msg    string
 }
 
-func worker(paths <-chan string, results chan<- Result, done chan<- bool) {
+func hasher(paths <-chan string, results chan<- result, done chan<- bool) {
 	for p := range paths {
 		data, err := ioutil.ReadFile(p)
 		if err != nil {
@@ -26,35 +26,64 @@ func worker(paths <-chan string, results chan<- Result, done chan<- bool) {
 		bn := path.Base(p)
 		sum := sha256.Sum256(data)
 		strsum := fmt.Sprintf("%x", sum)
-		ret := Result{true, fmt.Sprintf("%s OK", bn)}
+		ret := result{true, fmt.Sprintf("%s OK", bn)}
 		if bn != strsum {
-			ret = Result{false, fmt.Sprintf("%s CHECKSUM MISMATCH", bn)}
+			ret = result{false, fmt.Sprintf("%s CHECKSUM MISMATCH", bn)}
 		}
 		results <- ret
 	}
 	done <- true
 }
 
+func dirScanner(paths <-chan string, hasherQueue chan<- string, done chan<- bool) {
+	for p := range paths {
+		n := 0
+		err := filepath.Walk(p, func(path string, info os.FileInfo, err error) error {
+			if info == nil {
+				fmt.Printf("%s: [%v]\n", path, err)
+				return nil
+			}
+			if !info.Mode().IsRegular() {
+				return nil
+			}
+			n++
+			hasherQueue <- path
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("%s: walk error [%v]\n", p, err)
+		}
+	}
+	done <- true
+}
+
 func main() {
+
 	flag.Parse()
 	if flag.NArg() < 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
-	numWorkers := runtime.NumCPU()
-	fmt.Printf("Running with %d workers\n", numWorkers)
+	numHashers := runtime.NumCPU()
+	numDirScanners := 16
+	fmt.Printf("Running with %d hashers and %d parallel dir scanners\n", numHashers, numDirScanners)
 	rootpath := flag.Arg(0)
-	jobs := make(chan string, 100)
-	results := make(chan Result, 100)
-	done := make(chan bool, 100)
 
-	for w := 1; w <= numWorkers; w++ {
-		go worker(jobs, results, done)
+	hasherChannel := make(chan string, 100)
+	dirsChannel := make(chan string, 100)
+	results := make(chan result, 100)
+	hashersDone := make(chan bool, 100)
+	dirScannersDone := make(chan bool, 100)
+
+	for w := 1; w <= numHashers; w++ {
+		go hasher(hasherChannel, results, hashersDone)
+	}
+
+	for w := 1; w <= numDirScanners; w++ {
+		go dirScanner(dirsChannel, hasherChannel, dirScannersDone)
 	}
 
 	go func() {
-		parallelScanners := 0
-		waitChan := make(chan bool, 100)
 		// do a 1-deep parallel scan
 		files, err := ioutil.ReadDir(rootpath)
 		if err != nil {
@@ -63,35 +92,19 @@ func main() {
 		}
 		for _, file := range files {
 			if file.Mode().IsDir() {
-				np := path.Join(rootpath, file.Name())
-				parallelScanners += 1
-				go func() {
-					err := filepath.Walk(np, func(path string, info os.FileInfo, err error) error {
-						if info == nil {
-							fmt.Printf("%s: [%v]\n", path, err)
-							return nil
-						}
-						if !info.Mode().IsRegular() {
-							return nil
-						}
-						jobs <- path
-						return nil
-					})
-					if err != nil {
-						fmt.Printf("%s: walk error [%v]\n", np, err)
-					}
-					waitChan <- true
-				}()
+				p := path.Join(rootpath, file.Name())
+				dirsChannel <- p
 			}
 		}
-		fmt.Printf("Waiting for %d parallel scanners\n", parallelScanners)
-		// wait for paralle scanners
-		for w := 0; w < parallelScanners; w++ {
-			<-waitChan
+		close(dirsChannel)
+		// now wait for the dir scanners to be done
+		for i := 1; i <= numDirScanners; i++ {
+			<-dirScannersDone
 		}
-		close(jobs)
+		close(hasherChannel)
 	}()
 
+	printerDone := make(chan bool, 1)
 	go func() {
 		n := 0
 		for r := range results {
@@ -102,13 +115,20 @@ func main() {
 			if n%10 == 0 {
 				fmt.Printf("\r%d", n)
 			}
-			n += 1
+			n++
 		}
-		fmt.Printf("\n")
+		fmt.Printf("\rchecked %d files\n", n)
+		printerDone <- true
+		close(printerDone)
 	}()
 
-	// wait for all workers
-	for w := 1; w <= numWorkers; w++ {
-		<-done
+	fmt.Printf("Waiting for hashers...\n")
+	// wait for all hashers
+	for w := 1; w <= numHashers; w++ {
+		<-hashersDone
 	}
+	close(results)
+	// wait for printer
+	<-printerDone
+	fmt.Printf("done\n")
 }
